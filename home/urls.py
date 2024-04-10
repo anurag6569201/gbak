8075f2f3b715e15639b7f46cf701e4837bd70562
anurag singh <anurag6569201@gmail.com>
2024-04-11 01:24:04 +0530

    added templete update

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,11 @@ app_name="home"
 
 urlpatterns=[
     path("",views.index,name="index"),
+    path("about",views.about,name="about"),
+    path("causes",views.causes,name="causes"),
+    path("donate",views.donate,name="donate"),
+    path("blog",views.blog,name="blog"),
+    path("gallery",views.gallery,name="gallery"),
+    path("event",views.event,name="event"),
+    path("contact",views.contact,name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import redirect
 # Create your views here.
 def index(request):
     return render(request,"core/index.html")
+
+def about(request):
+    return render(request,"core/about.html")
+
+def causes(request):
+    return render(request,"core/causes.html")
+
+def donate(request):
+    return render(request,"core/donate.html")
+
+def blog(request):
+    return render(request,"core/blog.html")
+
+def gallery(request):
+    return render(request,"core/gallery.html")
+
+def event(request):
+    return render(request,"core/event.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
