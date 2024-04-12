825017afd47b3b91a61903e5863e00e6dd4a9fc0
anurag singh <anurag6569201@gmail.com>
2024-04-12 10:30:39 +0530

    added model for index page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home.models import homePageImage,investor,schoolnums
 
 # Register your models here.
+admin.site.register(schoolnums)
+admin.site.register(investor)
+admin.site.register(homePageImage)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class schoolnums(models.Model):
+    numberOfSchool=models.IntegerField()
+    states=models.IntegerField()
+    video = models.FileField(upload_to='videos/', null=True, blank=True)
+
+class investor(models.Model):
+    invName=models.CharField(max_length=50)
+    invDonationTime=models.CharField(max_length=50)
+    invMoney=models.CharField(max_length=10)
+    invPurpose=models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='investors/',null=True, blank=True)
+
+class homePageImage(models.Model):
+    images = models.ImageField(upload_to='HomeImages/',null=True, blank=True)
